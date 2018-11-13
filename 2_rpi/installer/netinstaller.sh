4958728bb9bb9a602689508ed255b9362fbe3afe
RedHog <egil@innovationgarage.no>
2018-11-13 14:19:43 +0000

    Bugfix for netinstaller

--- 2_rpi/installer/netinstaller.sh
+++ 2_rpi/installer/netinstaller.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
+sudo apt update
 sudo apt install -y git
 
-git clone git@github.com:innovationgarage/ElCheapoAIS.git
+git clone "https://github.com/innovationgarage/ElCheapoAIS.git"
 cd ElCheapoAIS/2_rpi/installer
 sudo ./setmyais.sh
