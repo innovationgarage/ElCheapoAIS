216e771623c9468406abe8d44ee85d2706866106
Egil Möller <egil.moller@piratpartiet.se>
2019-01-15 14:43:34 +0100

    Update elcheapoais-receiver.sh

--- 2_rpi/installer/elcheapoais-receiver.sh
+++ 2_rpi/installer/elcheapoais-receiver.sh
@@ -7,5 +7,4 @@ while : ; do
     LOG="/var/log/elcheapoais/ais.$(date +%Y-%m-%dT%H:%M).log"
     echo "Using device with serial $SERIAL..." > "$LOG"
     rtl_ais -n -T -P 1221 -p $PPM -g 60 -S 60  &>> "$LOG"
-    sleep 1
 done
