fa87d877198fa5675892c1353f30b1787faba8f3
Egil Moeller <egil@innovationgarage.no>
2019-01-17 13:15:26 +0000

    bugfix

--- 2_rpi/installer/elcheapoais-receiver.sh
+++ 2_rpi/installer/elcheapoais-receiver.sh
@@ -7,8 +7,8 @@ while : ; do
     LOG="/var/log/elcheapoais/ais.$(date +%Y-%m-%dT%H:%M).log"
     {
       echo "Using device with serial $SERIAL..."
-      echo "Running rtl_ais -n -h localhost -P 1221 -p $PPM"
-      rtl_ais -n -h localhost -P 1221 -p $PPM 2>&1
+      echo "Running rtl_ais -n -h 127.0.0.1 -P 1221 -p $PPM"
+      rtl_ais -n -h 127.0.0.1 -P 1221 -p $PPM 2>&1
     } > "$LOG"
     sleep 1
 done
